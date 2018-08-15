e843ab83c3f2d348ac61ca08e4bba3ce9200b51c
Måns Beckman <mans.beckman@spatialillusions.com>
2018-08-15 18:02:12 +0200

    Remove ;

--- tileserver/stop-update-start.sh
+++ tileserver/stop-update-start.sh
@@ -6,4 +6,4 @@ docker rm tileserver
 # update geodata
 node update-data.js
 # Restart container
-docker run --restart always --name tileserver -d -v $(pwd):/data -p 80:80 klokantech/tileserver-gl -c config-custom.json;
+docker run --restart always --name tileserver -d -v $(pwd):/data -p 80:80 klokantech/tileserver-gl -c config-custom.json
